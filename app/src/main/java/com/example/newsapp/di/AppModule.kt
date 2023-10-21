7ee8e9dcb0f16fe52be1546e6a8fbd31e1a10038
Paarkavi Priya S <paarkavi19092001@gmail.com>
2023-10-21 23:43:40 +0530

    [ADD] Shimmer effect added for article display- Home Screen

--- app/src/main/java/com/example/newsapp/di/AppModule.kt
+++ app/src/main/java/com/example/newsapp/di/AppModule.kt
@@ -2,14 +2,22 @@ package com.example.newsapp.di
 
 import android.app.Application
 import com.example.newsapp.data.manager.LocalUserManagerImpl
+import com.example.newsapp.data.remote.NewsApi
+import com.example.newsapp.data.repository.NewsRepositoryImpl
 import com.example.newsapp.domain.manager.LocalUserManager
+import com.example.newsapp.domain.repository.NewsRepository
 import com.example.newsapp.domain.usecases.app_entry.AppEntryUseCases
 import com.example.newsapp.domain.usecases.app_entry.ReadAppEntry
 import com.example.newsapp.domain.usecases.app_entry.SaveAppEntry
+import com.example.newsapp.domain.usecases.news.GetNews
+import com.example.newsapp.domain.usecases.news.NewsUseCases
+import com.example.newsapp.util.Constants.BASE_URL
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -30,5 +38,31 @@ object AppModule {
         readAppEntry = ReadAppEntry(localUserManger),
         saveAppEntry = SaveAppEntry(localUserManger)
     )
+@Provides
+@Singleton
+fun provideNewsApi() : NewsApi{
+    return Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(NewsApi::class.java)
+
+}
+
+
+    @Provides
+    @Singleton
+    fun provideNewsRepository(
+        newsApi: NewsApi
+    ): NewsRepository = NewsRepositoryImpl(newsApi)
+    @Provides
+    @Singleton
+    fun provideNewsUseCases(
+        newsRepository: NewsRepository
+    ) : NewsUseCases{
+        return NewsUseCases(
+            getNews = GetNews(newsRepository)
+        )
+    }
 
 }
--- app/src/main/java/com/example/newsapp/util/Constants.kt
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -6,4 +6,6 @@ object Constants {
     const val APP_ENTRY = "appEntry"
 
     const val API_KEY = "5c9e8b173fe74432bf678860f001c569"
+
+    const val BASE_URL = "https://newsapi.org/v2/"
 }
