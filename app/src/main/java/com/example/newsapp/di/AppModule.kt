424570b8427859fece2b73c17a12772319cd58a6
Paarkavi Priya S <paarkavi19092001@gmail.com>
2023-10-22 17:49:27 +0530

    [ADD] RoomDatabase implemented for bookmark functionality

--- app/src/main/java/com/example/newsapp/di/AppModule.kt
+++ app/src/main/java/com/example/newsapp/di/AppModule.kt
@@ -1,6 +1,10 @@
 package com.example.newsapp.di
 
 import android.app.Application
+import androidx.room.Room
+import com.example.newsapp.data.local.NewsDao
+import com.example.newsapp.data.local.NewsDatabase
+import com.example.newsapp.data.local.NewsTypeConvertor
 import com.example.newsapp.data.manager.LocalUserManagerImpl
 import com.example.newsapp.data.remote.NewsApi
 import com.example.newsapp.data.repository.NewsRepositoryImpl
@@ -9,9 +13,14 @@ import com.example.newsapp.domain.repository.NewsRepository
 import com.example.newsapp.domain.usecases.app_entry.AppEntryUseCases
 import com.example.newsapp.domain.usecases.app_entry.ReadAppEntry
 import com.example.newsapp.domain.usecases.app_entry.SaveAppEntry
+import com.example.newsapp.domain.usecases.news.DeleteArticle
 import com.example.newsapp.domain.usecases.news.GetNews
 import com.example.newsapp.domain.usecases.news.NewsUseCases
+import com.example.newsapp.domain.usecases.news.SearchNews
+import com.example.newsapp.domain.usecases.news.SelectArticle
+import com.example.newsapp.domain.usecases.news.UpsertArticle
 import com.example.newsapp.util.Constants.BASE_URL
+import com.example.newsapp.util.Constants.NEWS_DATABASE_NAME
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -58,11 +67,38 @@ fun provideNewsApi() : NewsApi{
     @Provides
     @Singleton
     fun provideNewsUseCases(
-        newsRepository: NewsRepository
+        newsRepository: NewsRepository,
+        newsDao: NewsDao
     ) : NewsUseCases{
         return NewsUseCases(
-            getNews = GetNews(newsRepository)
+            getNews = GetNews(newsRepository),
+            searchNews = SearchNews(newsRepository),
+            upsertArticle = UpsertArticle(newsDao),
+            deleteArticle = DeleteArticle(newsDao),
+            selectArticle = SelectArticle(newsDao)
+
         )
     }
+
+
+    @Provides
+    @Singleton
+    fun provideNewsDatabase(
+        application: Application
+    ): NewsDatabase{
+        return Room.databaseBuilder(
+            context = application,
+            klass = NewsDatabase::class.java,
+            name = NEWS_DATABASE_NAME
+        ).addTypeConverter(NewsTypeConvertor())
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideNewsDao(
+        newsDatabase: NewsDatabase
+    ) : NewsDao = newsDatabase.newsDao
 
 }
---
+++ app/src/main/java/com/example/newsapp/domain/usecases/news/SelectArticle.kt
@@ -0,0 +1,15 @@
+package com.example.newsapp.domain.usecases.news
+
+import com.example.newsapp.data.local.NewsDao
+import com.example.newsapp.domain.model.Article
+import kotlinx.coroutines.flow.Flow
+
+
+class SelectArticle(
+    private val newsDao: NewsDao
+) {
+     operator fun invoke(): Flow<List<Article>>{
+       return newsDao.getArticles()
+
+    }
+}
